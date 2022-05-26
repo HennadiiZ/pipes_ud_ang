455ce60ebf8512f6096869787fa80e6301457638
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-05-26 17:42:05 +0300

    working w/ arguments in pipes. in my way

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -7,11 +7,20 @@ export class ConvertPipe implements PipeTransform {
 
   transform(value: number, ...args: unknown[]): unknown {
     console.log(value)
+    console.log(args[0])
 
     if(!value){
        return
     }
+
+    if(args[0] === "m"){
+      return value * 1609.34;
+    }
     
+    if(args[0] === "mm"){
+      return value * 1609339.99997549;
+    }
+
     return value * 1.60934;
   }
 
